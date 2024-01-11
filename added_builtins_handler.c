5e4b9ac7888222fc66c20db3d20a16f99a4d70b5
dohaMST <mastourdoha.dev001@gmail.com>
2024-01-10 17:57:57 -0800

    10 change

--- added_builtins_handler.c
+++ added_builtins_handler.c
@@ -98,17 +98,17 @@ void cd_builtin(char **cmd, int *status)
 
 void handle_printenv(char **cmd, int *status)
 {
-	unsigned int i = 0;
+	char **env_ptr = environ;
 	(void)status;
 
 	/* Loop through the environment variables and print them */
-	while (environ[i])
+	while (*env_ptr)
 	{
 		/* write(STDOUT_FILENO, *env_ptr, strlen(*env_ptr)); */
-		_puts(environ[i]);
+		_puts(*env_ptr);
 		/* write(STDOUT_FILENO, "\n", 1); */
 		_puts("\n");
-		i++;
+		env_ptr++;
 	}
 	freeArr(cmd);
 	/* Set the exit status to 0 (success) */
