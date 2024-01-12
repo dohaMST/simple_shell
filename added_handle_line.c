65d934bc4f3bf048c34343c80ee11b9f48580ee2
dohaMST <mastourdoha.dev001@gmail.com>
2024-01-12 04:27:50 -0800

    02chack

--- added_handle_line.c
+++ added_handle_line.c
@@ -7,21 +7,20 @@
 
 char *read_line(void)
 {
-        char *cmd = NULL;
-        size_t len = 0;
-        ssize_t i;
+	char *cmd = NULL;
+	size_t len = 0;
+	ssize_t i;
 
-        if (isatty(STDIN_FILENO))
+	if (isatty(STDIN_FILENO))
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+	}
+	i = getline(&cmd, &len, stdin);
+	if (i == -1)
         {
-                 write(STDOUT_FILENO, "$ ", 2);
-        }
-        i = getline(&cmd, &len, stdin);
-
-        if (i == -1)
-        {
-                free(cmd);
-		return (NULL);
-        /*        exit(EXIT_SUCCESS);*/
-        }
-        return (cmd);
+		free(cmd);
+		/*return (NULL);*/
+		exit(EXIT_SUCCESS);
+	}
+	return (cmd);
 }
